b1eefd3e6d9143843d516df8a985ee65d78d387b
zh <593394100@qq.com>
2016-10-29 18:21:35 +0800

    显示cv 并显示cell

--- CoverFlow/CoverFlow/ViewController.m
+++ CoverFlow/CoverFlow/ViewController.m
@@ -8,22 +8,82 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+// 重用标识符
+static NSString *cellId = @"cellID";
+
+@interface ViewController () <UICollectionViewDataSource>
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    
+    /**
+     *  用来保存,3张美图图片!
+     */
+    NSArray<UIImage *> *_imgsArr;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupUI];
+    
+    [self loadData];
+}
+
+#pragma mark - 数据源方法
+// 返回item的个数!
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 30;
+}
+
+// 返回cell!
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    // 1.创建cell
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellId forIndexPath:indexPath];
+    
+    // 2.设置数据
+    cell.backgroundColor = [UIColor redColor];
+  
+    // 3.返回cell
+    return cell;
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma mark - 加载数据
+- (void)loadData {
+    
+    _imgsArr = @[
+                 [UIImage imageNamed:@"01"],
+                 [UIImage imageNamed:@"02"],
+                 [UIImage imageNamed:@"03"],
+                 ];
 }
+
+#pragma mark - 搭建界面
+- (void)setupUI {
+    
+    // MARK: - 1.添加collectionView
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    
+    UICollectionView *cv = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width, 200) collectionViewLayout:flowLayout];
+   
+    cv.backgroundColor = [UIColor lightGrayColor];
+    
+    // 设置数据源对象
+    cv.dataSource = self;
+    
+    // 注册cell
+    
+    [cv registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:cellId];
+   
+    [self.view addSubview:cv];
+    
+}
+
+
 
 
 @end
