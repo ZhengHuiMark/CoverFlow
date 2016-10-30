55f80582718434c9d531c4aefe93207b462c0455
zh <593394100@qq.com>
2016-10-30 10:17:22 +0800

    系统报错修复演示

--- CoverFlow/CoverFlow/ZHCoverFlowLayout.m
+++ CoverFlow/CoverFlow/ZHCoverFlowLayout.m
@@ -28,4 +28,31 @@
     
 }
 
+#pragma mark - 布局一定却与的cell
+- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
+    
+    // 1.先获取系统布局好的结果!
+    NSArray<UICollectionViewLayoutAttributes *> *oldAttrsArr = [super layoutAttributesForElementsInRect:rect];
+    
+    // 2.遍历集合,进行修改
+    // 2.1 临时集合
+    NSMutableArray *tempArrM = [NSMutableArray array];
+    
+    // 2.2 遍历集合,修改属性
+    [oldAttrsArr enumerateObjectsUsingBlock:^(UICollectionViewLayoutAttributes * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        // 2.2.1 修改之前先copy!
+        UICollectionViewLayoutAttributes *newAttr = obj.copy;
+        
+        newAttr.transform3D = CATransform3DRotate(newAttr.transform3D, M_PI_4, 0, 1, 0);
+        
+        // 2.2.x 保存到临时集合
+        [tempArrM addObject:newAttr];
+    }];
+    
+    // 3.返回
+    return tempArrM;
+
+}
+
 @end
