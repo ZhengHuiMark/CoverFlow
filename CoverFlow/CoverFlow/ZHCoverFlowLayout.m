cd4085338fa8fb3701a2f958572e5ac1ddda50b3
zh <593394100@qq.com>
2016-10-30 10:20:35 +0800

    计算距离进行缩放

--- CoverFlow/CoverFlow/ZHCoverFlowLayout.m
+++ CoverFlow/CoverFlow/ZHCoverFlowLayout.m
@@ -44,8 +44,31 @@
         // 2.2.1 修改之前先copy!
         UICollectionViewLayoutAttributes *newAttr = obj.copy;
         
-        newAttr.transform3D = CATransform3DRotate(newAttr.transform3D, M_PI_4, 0, 1, 0);
+//        newAttr.transform3D = CATransform3DRotate(newAttr.transform3D, M_PI_4, 0, 1, 0);
+        // 2.2.2 修改属性! -> 缩放 & 旋转!
+        // MARK: - 1.缩放
+        // - 1.屏幕中线的位置
+        CGFloat screenCenterX = self.collectionView.bounds.size.width * 0.5 + self.collectionView.contentOffset.x;
         
+        // - 2.每个cell的中心的x!!
+        CGFloat itemCenterX = newAttr.center.x;
+        
+        // - 3.计算距离
+        CGFloat distance = screenCenterX - itemCenterX;
+        
+        // - 4.将距离转换成缩放的比例! ABS() -> 取绝对值!
+        CGFloat scale = 1 - ABS(distance) / self.collectionView.bounds.size.width;
+        
+        // - 单位矩阵
+        CATransform3D transform = CATransform3DIdentity;
+        
+        
+        // - 缩放
+        transform = CATransform3DScale(transform, scale, scale, 1);
+        
+        // 赋值!
+        newAttr.transform3D = transform;
+
         // 2.2.x 保存到临时集合
         [tempArrM addObject:newAttr];
     }];
@@ -54,5 +77,15 @@
     return tempArrM;
 
 }
+
+#pragma mark - 只要显示的区域发生变化,就重新计算布局!
+// Invalidate 失效! 返回YES! 只要显示的区域发生改变,就让布局失效!
+// 重新计算布局!
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    
+    return YES;
+}
+
+
 
 @end
