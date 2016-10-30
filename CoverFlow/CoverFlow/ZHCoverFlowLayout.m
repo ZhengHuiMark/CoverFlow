2618c3570e30e7489f4587991e0d8aa988d70855
zh <593394100@qq.com>
2016-10-30 10:25:08 +0800

    实现停留位置设置 设置内边距,保证第一个和最后一个正常显示!

--- CoverFlow/CoverFlow/ZHCoverFlowLayout.m
+++ CoverFlow/CoverFlow/ZHCoverFlowLayout.m
@@ -26,6 +26,12 @@
     // 3.设置间距
     self.minimumLineSpacing = 0;
     
+    // 4.设置内边距
+    CGFloat inset = (self.collectionView.bounds.size.width - itemW) * 0.5;
+    self.sectionInset = UIEdgeInsetsMake(0, inset, 0, inset);
+    
+
+    
 }
 
 #pragma mark - 布局一定却与的cell
@@ -100,6 +106,51 @@
     
     return YES;
 }
+
+#pragma mark - 计算停留的位置
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity {
+    
+    // 1.获取系统计算好的值
+    CGPoint point = [super targetContentOffsetForProposedContentOffset:proposedContentOffset withScrollingVelocity:velocity];
+    
+    // 2.计算屏幕可见区域 -> CGRect!
+    CGRect rect;
+    rect.size = self.collectionView.bounds.size;
+#warning - 预计停留的位置时,内容的偏移量!
+    rect.origin = proposedContentOffset;
+    
+    // 3.获取可见区域内的cell!
+#warning - 利用上面的方法,计算可见区域内的cell!
+    NSArray<UICollectionViewLayoutAttributes *> *visibleAttrs = [self layoutAttributesForElementsInRect:rect];
+    
+    // 4.需要找出其中最近的cell!
+    // 4.2 遍历,找出距离屏幕中线最近的cell对应的attr
+    // - 屏幕中线的x
+    CGFloat screenCenterX = proposedContentOffset.x + self.collectionView.bounds.size.width * 0.5;
+    
+    // - 定义一个最小的间距
+    __block CGFloat minMargin = CGFLOAT_MAX;
+    // - 定义一个索引,用来记录最近那个attr的在数组中!索引!
+    __block NSInteger index = -1;
+    
+    [visibleAttrs enumerateObjectsUsingBlock:^(UICollectionViewLayoutAttributes * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        CGFloat distance = screenCenterX - obj.center.x;
+        
+        if (ABS(distance) < minMargin) {
+            minMargin = ABS(distance);
+            index = idx;
+        }
+        
+    }];
+    
+    // 5.计算需要偏移的距离!
+    CGFloat offsetX = screenCenterX - visibleAttrs[index].center.x;
+    
+    // 2.返回点坐标
+    return CGPointMake(point.x - offsetX, point.y);
+}
+
 
 
 
